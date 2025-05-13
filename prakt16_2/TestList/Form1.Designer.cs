529c9f8d2a725a5cb68aca797add7df1892f348d
BogdanKur <158187033+BogdanKur@users.noreply.github.com>
2025-05-14 00:59:16 +0300

    Add files via upload

---
+++ prakt16_2/TestList/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TestList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.memberList = new System.Windows.Forms.CheckedListBox();
+            this.peopleList = new System.Windows.Forms.ComboBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_sort = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.memberList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список участников";
+            // 
+            // memberList
+            // 
+            this.memberList.FormattingEnabled = true;
+            this.memberList.Location = new System.Drawing.Point(6, 21);
+            this.memberList.Name = "memberList";
+            this.memberList.Size = new System.Drawing.Size(188, 89);
+            this.memberList.TabIndex = 1;
+            // 
+            // peopleList
+            // 
+            this.peopleList.FormattingEnabled = true;
+            this.peopleList.Items.AddRange(new object[] {
+            "Курашов Б.Ю",
+            "Бугаева А.Д",
+            "Часовская А.В"});
+            this.peopleList.Location = new System.Drawing.Point(13, 214);
+            this.peopleList.Name = "peopleList";
+            this.peopleList.Size = new System.Drawing.Size(121, 24);
+            this.peopleList.TabIndex = 2;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(179, 132);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(95, 23);
+            this.button_add.TabIndex = 3;
+            this.button_add.Text = "Добавить";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(179, 161);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(95, 23);
+            this.button_delete.TabIndex = 4;
+            this.button_delete.Text = "Удалить";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // button_sort
+            // 
+            this.button_sort.Location = new System.Drawing.Point(179, 190);
+            this.button_sort.Name = "button_sort";
+            this.button_sort.Size = new System.Drawing.Size(95, 23);
+            this.button_sort.TabIndex = 5;
+            this.button_sort.Text = "Сортировка";
+            this.button_sort.UseVisualStyleBackColor = true;
+            this.button_sort.Click += new System.EventHandler(this.button_sort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 259);
+            this.Controls.Add(this.button_sort);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.peopleList);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Работа со списками";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox memberList;
+        private System.Windows.Forms.ComboBox peopleList;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_sort;
+    }
+}
+
